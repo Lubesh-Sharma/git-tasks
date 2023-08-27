3a5d4fb242d20c45e9c1e2686f5c0ca02d5f793d
Lubesh <lubeshkumarsharma@gmail.com>
2023-08-27 23:28:56 +0530

    This is Error Commit

---
+++ dummy_code.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<math.h>
+#include<stdlib.h>
+#include<string.h>
+int main()
+{
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    #endif
+
+    printf("Hello all.\n This is my task3\n");
+
+    return 0;
+}
